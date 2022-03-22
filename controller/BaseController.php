4fbca69b98f755dc33c83a390794f5eebb07404f
pablogallero <pablogallero@gmail.com>
2022-03-22 10:54:18 +0100

    Update BaseController.php

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -44,7 +44,7 @@ class BaseController {
 			$this->currentUser = new User($_SESSION["currentuser"]);
 			//add current user to the view, since some views require it
 			$this->view->setVariable("currentusername",
-					$this->currentUser->getUsername());
+					$this->currentUser->getEmail());
 		}
 	}
 }
